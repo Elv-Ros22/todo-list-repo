5d1be4a4c9303d8a9acfe9f0e6baa52018120b5a
Elv-Ros22 <elviamanrique00@gmail.com>
2025-05-12 22:49:15 -0500

    Added wraps for App.js with Router for nav

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Todo from "./components/Todo";
 import ContactFrom from "./components/ContactForm";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text, completed: false }]);
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const editTodo = (id, newText) => {
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, text: newText } : todo))
+    );
+  };
+
   return (
-    <Router>
+    <>
       <Navbar />
-      <nav>
-        <Link to="/todo">Todo</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
       <Routes>
-        <Route path="/todo" element={<Todo />} />
+        <Route
+          path="/todos"
+          element={
+            <Todo
+              todos={todos}
+              addTodo={addTodo}
+              deleteTodo={deleteTodo}
+              editTodo={editTodo}
+            />
+          }
+        />
         <Route path="/contact" element={<ContactFrom />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 export default App;

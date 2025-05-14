b1549d54ead79b835d44775d97cd94b64ef7bd59
Elv-Ros22 <elviamanrique00@gmail.com>
2025-05-13 20:29:20 -0500

    revised file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Todo from "./components/Todo";
 import ContactFrom from "./components/ContactForm";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (text) => {
-    setTodos([...todos, { id: Date.now(), text, completed: false }]);
-  };
-
   return (
-    <>
+    <div>
       <Navbar />
+
       <Routes>
-        <Route
-          path="/todos"
-          element={<Todo todos={todos} addTodo={addTodo} />}
-        />
+        <Route path="/todos" element={<Todo />} />
         <Route path="/contact" element={<ContactFrom />} />
       </Routes>
-    </>
+    </div>
   );
 }
 export default App;
